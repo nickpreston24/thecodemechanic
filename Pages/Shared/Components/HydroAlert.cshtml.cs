75581815048190c1aecaa9cd329dde7be86ece9f
nickpreston24 <nicholas.preston24@gmail.com>
2024-12-21 23:47:23 -0600

    fix it, you fruitcakes!

--- Pages/Shared/Components/HydroAlert.cshtml.cs
+++ Pages/Shared/Components/HydroAlert.cshtml.cs
@@ -5,5 +5,5 @@ namespace thecodemechanic.Pages.Shared.Components;
 public class HydroAlert : HydroView
 {
     public string message { get; set; } = string.Empty;
-    public Exception ex { get; set; }
+    public Exception ex { get; set; } = new Exception(string.Empty);
 }
