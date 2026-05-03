88030d3f85e4e5be56139da89bdbdfd0a633fa1e
nickpreston24 <nicholas.preston24@gmail.com>
2026-05-02 22:16:21 -0400

    rm sqil generated code (for now); bumped packages

--- Pages/Samples/JustDoIt/Index.cshtml.cs
+++ Pages/Samples/JustDoIt/Index.cshtml.cs
@@ -42,26 +42,28 @@ public class Index : PageModel
         return Partial("_Edit", Database.FirstOrDefault(p => p.Id == Id));
     }
 
-    public async Task<IActionResult> OnPostUpdate([FromForm] Todo todo)
-    {
-        int rows = await Upsert(todo);
+    // TODO: Uncomment these once you finish sqil's codegenerators
 
-        // if nothing is updated, throw:
-        if (rows == 0 || Database.FirstOrDefault(x => x.Id == Id) is not { } t)
-            return BadRequest();
+    // public async Task<IActionResult> OnPostUpdate([FromForm] Todo todo)
+    // {
+    //     int rows = await Upsert(todo);
+    //
+    //     // if nothing is updated, throw:
+    //     if (rows == 0 || Database.FirstOrDefault(x => x.Id == Id) is not { } t)
+    //         return BadRequest();
+    //
+    //     // update the table row:
+    //     t.Content = todo.Content;
+    //     t.Id = todo.Id;
+    //
+    //     return Request.IsHtmx() ? Partial("_Row", t) : Redirect("Index");
+    // }
 
-        // update the table row:
-        t.Content = todo.Content;
-        t.Id = todo.Id;
-
-        return Request.IsHtmx() ? Partial("_Row", t) : Redirect("Index");
-    }
-
-    private async Task<int> Upsert(Todo todo)
-    {
-        int rows = await Procs.upserttodo.UpsertAsync(todo);
-        return rows;
-    }
+    // private async Task<int> Upsert(Todo todo)
+    // {
+    //     int rows = await Procs.upserttodo.UpsertAsync(todo);
+    //     return rows;
+    // }
 
     public List<Todo> Todos => Database;
 }
